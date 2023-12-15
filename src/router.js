d444db18e4fe64f03c9d884c1dabce137c8c7c1b
Andrew <ak36794ca@gmail.com>
2023-12-15 08:36:24 +0200

    fixed router, replaced by createHashRouter

--- src/router.js
+++ src/router.js
@@ -1,9 +1,10 @@
-import {createBrowserRouter, Navigate} from "react-router-dom";
+import { createHashRouter, Navigate} from "react-router-dom";
 import React from "react";
 import {MainPage} from "./pages";
 import {MainLayout} from "./layouts";
 
-const router= createBrowserRouter([
+// const router= createBrowserRouter([
+const router= createHashRouter([
     {
         path:"",element:<MainLayout/>, children:[
             {index:true,element:<Navigate to={"main"}/>},
